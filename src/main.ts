efdfcaa2f89ecdf1db9bf0f69b18c2c3b93bde19
bayst-custom <ivanova.tonia.2000@gmail.com>
2025-10-26 00:40:21 +0400

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 
     app.enableCors({
         // origin: 'http://localhost:3000',
-        origin: 'https://bayts-custom.github.io/admin-front/*',
+        origin: 'https://bayts-custom.github.io/*',
     });
 
     await app.listen(envConfig.server.port);
